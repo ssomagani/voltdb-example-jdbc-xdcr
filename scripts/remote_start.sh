d06b9ef7c745398343a80207500ef174bb40af60
ssomagani <ssomagani@voltdb.com>
2019-07-16 10:06:30 -0400

    Fixed the ignore option to 'thp' instead of 'THP'

--- scripts/remote_start.sh
+++ scripts/remote_start.sh
@@ -3,4 +3,4 @@
 scp -i /Users/seetasomagani/.ssh/ec2_rhel.pem -o 'IdentitiesOnly yes' /Users/seetasomagani/Projects/voltdb-example-jdbc-xdcr/config/deployment-$2.xml ec2-user@$1:/home/ec2-user/deployment.xml
 
 ssh -t -i /Users/seetasomagani/.ssh/ec2_rhel.pem -o 'IdentitiesOnly yes' ec2-user@$1 'voltdb-ent-9.0/bin/voltdb init -f -C deployment.xml'
-ssh -t -i /Users/seetasomagani/.ssh/ec2_rhel.pem -o 'IdentitiesOnly yes' ec2-user@$1 'voltdb-ent-9.0/bin/voltdb start -B --ignore=THP'
+ssh -t -i /Users/seetasomagani/.ssh/ec2_rhel.pem -o 'IdentitiesOnly yes' ec2-user@$1 'voltdb-ent-9.0/bin/voltdb start -B --ignore=thp'
